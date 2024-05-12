c8159027f99d02ad898ce3d0679bcdb623603579
Just4you <w0d@hotmail.com>
2024-05-13 02:52:04 +0300

    Update Powershell scripts after testing
    + Fixing packages dependencies

--- start.ps1
+++ start.ps1
@@ -1,15 +1,58 @@
-# Check if the current shell
-if ($env:CMDER_ROOT -eq "") {
-    $shell = "cmd"
-} else {
-    $shell = "powershell"
+# Define function to handle cleanup
+function Cleanup {
+    # Stop the client and server applications if running
+    Stop-Process -Name "node" -ErrorAction SilentlyContinue
+    Stop-Process -Name "react-scripts" -ErrorAction SilentlyContinue
+    exit 1
 }
 
-# Install npm dependencies for client and server
-Write-Host "Installing npm dependencies..."
-Start-Process -FilePath "yarn" -ArgumentList "--prefix", "client" -Wait
-Start-Process -FilePath "yarn" -ArgumentList "--prefix", "server" -Wait
+# Set up trap to catch interruptions
+trap { Cleanup; exit }
 
-# Run client and server applications in separate terminals
-Start-Process -FilePath $shell -ArgumentList "/k yarn --prefix client start"
-Start-Process -FilePath $shell -ArgumentList "/k yarn --prefix server start"
+# Define paths to client and server directories
+$clientDir = "client"
+$serverDir = "server"
+
+# Check if client and server directories exist
+if (-not (Test-Path $clientDir)) {
+    Write-Host "Client directory not found. Exiting..."
+    exit 1
+}
+
+if (-not (Test-Path $serverDir)) {
+    Write-Host "Server directory not found. Exiting..."
+    exit 1
+}
+
+if (-not (Test-Path $clientDir\node_modules)) {
+    Write-Host "Client dependencies not found. Installing..."
+    # Install npm dependencies for client
+    Write-Host "Installing dependencies for client..."
+    Start-Process -FilePath "npm" -ArgumentList "install --legacy-peer-deps" -WorkingDirectory $clientDir -Wait
+} else {
+    Write-Host "Client dependencies found. Skipping installation..."
+}
+
+if (-not (Test-Path $serverDir\node_modules)) {
+    Write-Host "Server dependencies not found. Installing..."
+    # Install npm dependencies for server
+    Write-Host "Installing dependencies for server..."
+    Start-Process -FilePath "npm" -ArgumentList "install" -WorkingDirectory $serverDir -Wait
+} else {
+    Write-Host "Server dependencies found. Skipping installation..."
+}
+
+# Start client application
+Write-Host "Starting client application..."
+Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $clientDir
+
+# Start server application
+Write-Host "Starting server application..."
+Start-Process -FilePath "npm" -ArgumentList "start" -WorkingDirectory $serverDir
+
+# Wait for user input to exit
+Write-Host "Press any key to exit..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+# Clean up resources
+Cleanup
